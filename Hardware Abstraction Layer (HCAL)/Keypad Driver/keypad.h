ee5a2e2b85008eff296216fbdf6adbd090d53693
mohanadsaeed <mohandkhaled24@gmail.com>
2021-02-12 20:57:56 +0200

    Implementing keypad driver which support 4*4 and 4*3 keypads

---
+++ Hardware Abstraction Layer (HCAL)/Keypad Driver/keypad.h
@@ -0,0 +1,40 @@
+/* -----------------------------------------------------------------------------
+[FILE NAME]    :	keypad.h
+
+[AUTHOR]       :	MOHANAD K. SAEED
+
+[DATA CREATED] :	12/02/2021
+
+[DESCRIPTION]  :	Header File for Keypad Driver
+------------------------------------------------------------------------------*/
+
+#ifndef KEYPAD_H
+#define KEYPAD_H
+
+#include "std_types.h"
+#include "avr_config.h"
+#include "common_macros.h"
+
+/* -----------------------------------------------------------------------------
+ *                      Preprocessor Macros                                    *
+ ------------------------------------------------------------------------------*/
+
+/* Keypad configurations for number of rows and columns */
+#define N_col 4
+#define N_row 4
+
+/* Keypad Port Configurations */
+#define KEYPAD_PORT_OUT PORTA
+#define KEYPAD_PORT_IN  PINA
+#define KEYPAD_PORT_DIR DDRA 
+
+/* -----------------------------------------------------------------------------
+ *                      Functions Prototypes                                   *
+/* -----------------------------------------------------------------------------
+
+/*
+ * Function responsible for getting the pressed keypad key
+ */
+uint8 KeyPad_getPressedKey(void);
+
+#endif
