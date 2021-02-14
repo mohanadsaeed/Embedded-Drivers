b05031e20c3bfb2bcb7288c30f5b007486683df5
mohanadsaeed <mohandkhaled24@gmail.com>
2021-02-14 17:14:09 +0200

    Implementing ADC driver for ATmega16 which supports Interrupt mode, polling mode and auto trigger mode

---
+++ Microconrtoller Abstraction Layer (MCAL)/AVR/ATmega16/ADC/adc.c
@@ -0,0 +1,86 @@
+/* -----------------------------------------------------------------------------
+[FILE NAME]    :	adc.h
+
+[AUTHOR]       :	MOHANAD K. SAEED
+
+[DATA CREATED] :	14/02/2021
+
+[DESCRIPTION]  :	ADC Driver
+------------------------------------------------------------------------------*/
+
+#include "adc.h"
+
+#ifdef INTERRUPT_MODE
+/* -----------------------------------------------------------------------------
+ *                          Global Variables                                   *
+------------------------------------------------------------------------------*/
+
+volatile uint16  g_adcResult = 0;
+
+/* -----------------------------------------------------------------------------
+ *                          ISR's Definitions                                  *
+------------------------------------------------------------------------------*/
+
+ISR(ADC_vect)
+{
+	/* Read ADC Data after conversion complete */
+	g_adcResult = ADC;
+}
+#endif
+
+void ADC_init(const Adc_ConfigType * config_Ptr){
+	/* ADMUX Register Bits Description:
+	 * REFS1:0 = 00 to choose to connect external reference voltage by input
+	 * this voltage through AREF pin
+	 * ADLAR   = 0 right adjusted
+	 * MUX4:0  = 00000 to choose channel 0 as initialization
+	 */
+	ADMUX = 0;
+
+	/* ADCSRA Register Bits Description:
+	 * ADEN    = 1 Enable ADC
+	 * ADIE    = 0 Disable ADC Interrupt
+	 * ADPS2:0 pre-scalar value should be assigned as the config_Ptr to be
+	 * configurable
+	 */
+	SET_BIT(ADCSRA,ADEN);
+	CLEAR_BIT(ADCSRA,ADATE);
+	CLEAR_BIT(ADCSRA,ADSC);
+
+#ifdef INTERRUPT_MODE
+	SET_BIT(ADCSRA,ADIE);
+	#ifdef AUTO_TRIGGER_MODE
+		SET_BIT(ADCSRA,ADATE);
+		SFIOR = (SFIOR & NUM_TO_CLEAR_LAST_3_BITS) |\
+				((config_Ptr -> triggersource & NUM_TO_CLEAR_LAST_5_BITS)<<5);
+	#endif
+#endif
+
+#ifdef POLLING_MODE
+	CLEAR_BIT(ADCSRA,ADIE);
+#endif
+
+	ADCSRA = (ADCSRA & NUM_TO_CLEAR_FIRST_3_BITS) | \
+			(config_Ptr -> clock & NUM_TO_CLEAR_LAST_5_BITS);
+
+}
+
+#ifdef INTERRUPT_MODE
+void ADC_readChannel(const Adc_ConfigType * config_Ptr){
+	ADMUX = (ADMUX & NUM_TO_CLEAR_FIRST_5_BITS) | \
+			(config_Ptr -> channelnumber & NUM_TO_CLEAR_LAST_5_BITS);
+	SET_BIT(ADCSRA,ADSC);		/* start conversion write '1' to ADSC */
+}
+#endif
+
+#ifdef POLLING_MODE
+uint16 ADC_readChannel(const Adc_ConfigType * config_Ptr){
+	ADMUX = (ADMUX & NUM_TO_CLEAR_FIRST_5_BITS) | \
+			(config_Ptr -> channelnumber & NUM_TO_CLEAR_LAST_5_BITS);
+	SET_BIT(ADCSRA,ADSC);		/* start conversion write '1' to ADSC */
+	while(BIT_IS_CLEAR(ADCSRA,ADIF)); /* wait for conversion to complete ADIF becomes '1' */
+	SET_BIT(ADCSRA,ADIF); /* clear ADIF by write '1' to it :) */
+	return ADC; /* return the data register */
+}
+#endif
+
---
+++ Microconrtoller Abstraction Layer (MCAL)/AVR/ATmega16/ADC/adc.h
@@ -0,0 +1,73 @@
+/* -----------------------------------------------------------------------------
+[FILE NAME]    :	adc.h
+
+[AUTHOR]       :	MOHANAD K. SAEED
+
+[DATA CREATED] :	14/02/2021
+
+[DESCRIPTION]  :	Header File for ADC Driver
+------------------------------------------------------------------------------*/
+
+#ifndef ADC_H
+#define ADC_H
+
+/* -----------------------------------------------------------------------------
+ *                           Includes                                          *
+ ------------------------------------------------------------------------------*/
+#include "std_types.h"
+#include "micro_config.h"
+#include "common_macros.h"
+
+/* -----------------------------------------------------------------------------
+ *                           ENUM & Structures                                          *
+ ------------------------------------------------------------------------------*/
+typedef enum
+{
+	F_CPU_1,F_CPU_2,F_CPU_4,F_CPU_8,F_CPU_16,F_CPU_32,F_CPU_64,F_CPU_128
+}Adc_Clock;
+
+typedef enum
+{
+	ADC0,ADC1,ADC2,ADC3,ADC4,ADC5,ADC6,ADC7,ADC0_ADC0_10x,ADC1_ADC0_10x,\
+	ADC0_ADC0_200x,ADC1_ADC0_200x,ADC2_ADC2_10x,ADC3_ADC2_10x,ADC2_ADC2_200x,\
+	ADC3_ADC2_200x,ADC0_ADC1,ADC1_ADC1,ADC2_ADC1,ADC3_ADC1,ADC4_ADC1,ADC5_ADC1,\
+	ADC6_ADC1,ADC7_ADC1,ADC0_ADC2,ADC1_ADC_2,ADC2_ADC2,ADC3_ADC2,ADC4_ADC2
+}Adc_ChannelNumber;
+
+typedef enum
+{
+	FREE_RUNNING_MODE,ANALOG_COMPARATOR,EXTERNAL_INTERRUPT_REQUEST0,\
+	TIMER0_COMPARE_MATCH,TIME0_OVERFLOW,TIME1_COMPARE_MATCH_B,TIME1_OVERFLOW,\
+	TIME1_CAPTURE_EVENT
+}Adc_TriggerSource;
+
+typedef struct
+{
+	Adc_Clock clock;
+	Adc_ChannelNumber channelnumber;
+	Adc_TriggerSource triggersource;
+}Adc_ConfigType;
+
+/* -----------------------------------------------------------------------------
+ *                      Preprocessor Macros                                    *
+ ------------------------------------------------------------------------------*/
+#define INTERRUPT_MODE
+#define NUM_TO_CLEAR_FIRST_3_BITS 0xF8
+#define NUM_TO_CLEAR_LAST_3_BITS 0x1F
+#define NUM_TO_CLEAR_LAST_5_BITS 0x07
+#define NUM_TO_CLEAR_FIRST_5_BITS 0xE0
+
+ 
+ /* -----------------------------------------------------------------------------
+ *                      Functions Declaration                                   *
+------------------------------------------------------------------------------*/
+void ADC_init(const Adc_ConfigType * config_Ptr);
+#ifdef INTERRUPT_MODE
+void ADC_readChannel(const Adc_ConfigType * config_Ptr);
+#endif
+#ifdef POLLING_MODE
+uint16 ADC_readChannel(const Adc_ConfigType * config_Ptr);
+#endif
+
+
+#endif
